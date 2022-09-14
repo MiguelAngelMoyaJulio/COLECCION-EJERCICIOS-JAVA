79708fca467224f5e74d237d338dc1a568e1e42a
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-09-14 16:06:02 -0300

    E3 conditional if - type of triangle

---
+++ EJERCICIOS/src/main/java/org/ar/miguel/E3.java
@@ -0,0 +1,26 @@
+package org.ar.miguel;
+
+import java.util.Scanner;
+
+/**
+ * Dado un triángulo representado por sus lados lado1, lado2 y lado3,
+ * determinar e indicar según corresponda:
+ * “equilátero”, “isósceles” o “escálenos”.
+ */
+public class E3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int l1, l2, l3;
+        System.out.println("enter the three sides of a triangle");
+        l1 = Integer.parseInt(s.nextLine());
+        l2 = Integer.parseInt(s.nextLine());
+        l3 = Integer.parseInt(s.nextLine());
+        if (l1 == l2 && l3 == l2) {
+            System.out.println("equilateral");
+        } else if (l1 == l2 || l3 == l2 || l3 == l1) {
+            System.out.println("isosceles");
+        } else {
+            System.out.println("scalene");
+        }
+    }
+}
