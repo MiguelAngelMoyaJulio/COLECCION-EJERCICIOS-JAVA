b3dd6080b3eb95a9376a9f6f4026ad1015bb8d14
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-09-16 14:05:32 -0300

    E11 prime number

---
+++ EJERCICIOS/src/main/java/org/ar/miguel/E11.java
@@ -0,0 +1,27 @@
+package org.ar.miguel;
+
+import java.util.Scanner;
+
+/**
+ * Desarrollar un programa tal que,
+ * dado un valor entero positivo que se ingresa
+ * por teclado indique si se trata de un número primo o no.
+ */
+public class E11 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int c = 0, n = 0;
+        System.out.println("enter a number ");
+        n = Integer.parseInt(s.nextLine());
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                c++;
+            }
+        }
+        if (c == 2) {
+            System.out.println("it's prime number");
+        } else {
+            System.out.println("it's not prime number");
+        }
+    }
+}
