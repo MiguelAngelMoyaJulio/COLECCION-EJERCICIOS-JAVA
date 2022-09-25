b90d5f74c66360399735ab2ff1508d290ef207aa
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-09-25 16:32:55 -0300

    E23 array mixing elements

---
+++ EJERCICIOS/src/main/java/org/ar/miguel/ARRAYS/E23.java
@@ -0,0 +1,58 @@
+package org.ar.miguel.ARRAYS;
+
+import java.util.Scanner;
+
+/**
+ * Ingresar un valor entero N (< 30).
+ * A continuación ingresar un conjunto UNO y luego otro conjunto DOS,
+ * ambos de N componentes.
+ * Generar e imprimir otro conjunto TRES intercalando
+ * los valores de posición impar de DOS y los valores
+ * de posición par de UNO.
+ * Diseñar un procedimiento que rellene un
+ * arreglo en posiciones salteadas con las componentes de
+ * otro y usarlo para cada conjunto.
+ */
+public class E23 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] uno;
+        int[] dos;
+        int[] tres;
+        int n = 0, suma = 0, num = 0;
+        int j = 0, k = 0;
+        System.out.println("enter the size of the array");
+        n = Integer.parseInt(s.nextLine());
+        if (n < 30) {
+            uno = new int[n];
+            dos = new int[n];
+            tres = new int[2 * n];
+            for (int i = 0; i < uno.length; i++) {
+                System.out.println("enter a number");
+                num = Integer.parseInt(s.nextLine());
+                uno[i] = num;
+            }
+            for (int i = 0; i < dos.length; i++) {
+                System.out.println("enter a number");
+                num = Integer.parseInt(s.nextLine());
+                dos[i] = num;
+            }
+
+            for (int i = 0; i < 2 * n; i++) {
+                if (i % 2 == 0) {
+                    tres[i] = uno[j];
+                    j++;
+                } else {
+                    tres[i] = dos[k];
+                    k++;
+                }
+            }
+            for (int i : tres) {
+                System.out.println(i);
+            }
+        } else {
+            System.out.println("Try again");
+        }
+
+    }
+}
