6d3cba0d84e3c147d747e6b5d3a756f6ac371315
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-09-27 14:49:44 -0300

    E31 CONTROL CUT - wifi

---
+++ EJERCICIOS/src/main/java/org/ar/miguel/FILES/CONTROL_CUT/E31/E31.java
@@ -0,0 +1,54 @@
+package org.ar.miguel.FILES.CONTROL_CUT.E30;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+/**
+ * Se cuenta con un archivo de Puntos WiFi públicos de la Ciudad de Buenos Aires, con el siguiente diseño de
+ * registro: COMUNA, SITIO, APS, NOMBRE, DIRECCIÓN NORMALIZADA, LAT, LONG
+ * Usando corte de control a partir de este archivo, deben generar un listado de puntos wifi agrupando por
+ * comuna y luego por sitio (Biblioteca, espacio público, etc).
+ */
+public class E31 {
+    public static void main(String[] args) {
+        String readRecord = null;
+        String[] record = new String[0];
+        String neighborhoodAnt = "";
+        String siteAnt = "";
+        int totalWifiPerNeighborhood = 0;
+        int totalWifiPerSite = 0;
+        try {
+            //Put the exact location of the file Datos.csv
+            FileReader fileReader = new FileReader("D:\\Miguel\\DEVELOPMENT\\JAVA-EJERCICIOS-GIT\\EJERCICIOS\\src\\main\\java\\org\\ar\\miguel\\FILES\\CONTROL_CUT\\E31\\LOTE.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            readRecord = bufferedReader.readLine();
+            while (readRecord != null) {
+                record = readRecord.split(";");
+                neighborhoodAnt = record[0];
+                totalWifiPerNeighborhood = 0;
+                System.out.println("PUNTO WIFI COMUNA : " + neighborhoodAnt);
+                while (neighborhoodAnt.equalsIgnoreCase(record[0]) && readRecord != null) {
+                    siteAnt = record[1];
+                    totalWifiPerSite = 0;
+                    while (siteAnt.equalsIgnoreCase(record[1]) && neighborhoodAnt.equalsIgnoreCase(record[0]) && readRecord != null) {
+                        totalWifiPerSite++;
+                        readRecord = bufferedReader.readLine();
+                        if (readRecord != null) {
+                            record = readRecord.split(";");
+                        }
+                    }
+                    System.out.println(siteAnt +":"+totalWifiPerSite);
+                    totalWifiPerNeighborhood = totalWifiPerNeighborhood + totalWifiPerSite;
+                }
+                System.out.println("TOTAL PUNTOS WIFI COMUNA "+neighborhoodAnt+" : "+totalWifiPerNeighborhood);
+                System.out.println("");
+            }
+            bufferedReader.close();
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
