3ab3e204abc47fda737620e578a44b7fef3929c5
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-10 16:59:04 -0300

    E15 SHOW N PRIME NUMBERS REVIEW

--- EJERCICIOS/src/main/java/org/ar/miguel/STRUCTURES/E15.java
+++ EJERCICIOS/src/main/java/org/ar/miguel/STRUCTURES/E15.java
@@ -11,8 +11,10 @@ public class E15 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n, pe=0,i=1;
+
         System.out.println("enter a number ");
         n = Integer.parseInt(s.nextLine());
+
         while(pe<n){
             if(isPrimeNumber(i)){
                 System.out.println(i);
@@ -25,11 +27,13 @@ public class E15 {
     static boolean isPrimeNumber(int num) {
         boolean prime = false;
         int c = 0;
+
         for (int i = 1; i <= num; i++) {
             if (num % i == 0) {
                 c++;
             }
         }
+
         if (c == 2) {
             prime = true;
         }
