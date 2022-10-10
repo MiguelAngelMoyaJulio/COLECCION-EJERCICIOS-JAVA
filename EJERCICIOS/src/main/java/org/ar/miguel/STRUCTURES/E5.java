63b10731347daf554481402b79335fb46d1224c9
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-10 12:36:22 -0300

    E5 SUCCESSIVE SUM REVIEW

--- EJERCICIOS/src/main/java/org/ar/miguel/STRUCTURES/E5.java
+++ EJERCICIOS/src/main/java/org/ar/miguel/STRUCTURES/E5.java
@@ -13,6 +13,10 @@ public class E5 {
         System.out.println("enter two numbers");
         a = Integer.parseInt(s.nextLine());
         b = Integer.parseInt(s.nextLine());
+        result(b, a, suma);
+    }
+
+    private static void result(int b, int a, int suma) {
         for (int i = 1; i <= b; i++) {
             suma = suma + a;
         }
