45ad74acd60b15d5e2b0c655d65f67771978c336
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-10 17:21:24 -0300

    E19 ARRAY - EVEN NUMBERS REVIEW

--- EJERCICIOS/src/main/java/org/ar/miguel/ARRAYS/E19.java
+++ EJERCICIOS/src/main/java/org/ar/miguel/ARRAYS/E19.java
@@ -10,25 +10,31 @@ import java.util.Scanner;
 public class E19 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+
         int[] arra;
         int n = 0, i = 1, j = 0;
+
         System.out.println("enter a number");
         n = Integer.parseInt(s.nextLine());
+
         if (n < 25) {
             arra = new int[n];
-            while (j < n) {
-                if (i % 2 == 0) {
-                    arra[j] = i;
-                    j++;
-                }
-                i++;
-            }
-            for (int x = 0; x < arra.length; x++) {
-                System.out.println(arra[x]);
-            }
+            result(n, arra, i, j);
         } else {
             System.out.println("Try again");
         }
     }
 
+    private static void result(int n, int[] arra, int i, int j) {
+        while (j < n) {
+            if (i % 2 == 0) {
+                arra[j] = i;
+                j++;
+            }
+            i++;
+        }
+        for (int x = 0; x < arra.length; x++) {
+            System.out.println(arra[x]);
+        }
+    }
 }
