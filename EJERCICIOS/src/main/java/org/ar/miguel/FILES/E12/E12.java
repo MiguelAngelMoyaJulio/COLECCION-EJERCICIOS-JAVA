a8c92505c1760bc0900e840b8d7c1fc0940842af
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-10 16:53:03 -0300

    E12 CONTAINERS REVIEW

--- EJERCICIOS/src/main/java/org/ar/miguel/FILES/E12/E12.java
+++ EJERCICIOS/src/main/java/org/ar/miguel/FILES/E12/E12.java
@@ -23,32 +23,37 @@ import java.util.Scanner;
  * •	La cantidad de contenedores que se trasladarán a cada puerto.
  */
 public class E12 {
-    private static final int PUERTO_1 = 1;
-    private static final int PUERTO_2 = 2;
-    private static final int PUERTO_3 = 3;
+    private final static int PUERTO_1 = 1;
+    private final static int PUERTO_2 = 2;
+    private final static int PUERTO_3 = 3;
 
     public static void main(String[] args) {
         DecimalFormat df = new DecimalFormat("###,###.##");
         double pesoTotal = 0d, contMayorPeso = 0d;
-        int p1 = 0, p2 = 0, p3 = 0;
-        String idPesoMax="";
+        int p1 = 0, p2 = 0, p3 = 0, i = 0;
+        String idPesoMax = "";
         String cadenaLectura = null;
         String[] stringsArray = new String[0];
-        int i = 0;
+
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+
         try {
             //Put the exact location of the file Datos.csv
-            FileReader fileReader = new FileReader("file's location");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            fileReader = new FileReader("file's location");
+            bufferedReader = new BufferedReader(fileReader);
             while ((cadenaLectura = bufferedReader.readLine()) != null) {
                 stringsArray = cadenaLectura.split(",");
+
                 String idCont = stringsArray[0];
                 double peso = Double.parseDouble(stringsArray[1]);
                 String idPuerto = stringsArray[2];
+
                 pesoTotal = pesoTotal + peso;
                 if (i == 0) {
                     contMayorPeso = peso;
+                    i++;
                 }
-                i++;
                 if (peso > contMayorPeso) {
                     contMayorPeso = peso;
                     idPesoMax = "";
@@ -57,22 +62,30 @@ public class E12 {
 
                 switch (Integer.parseInt(idPuerto)) {
                     case PUERTO_1:
-                        p1++;break;
+                        p1++;
+                        break;
                     case PUERTO_2:
-                        p2++;break;
+                        p2++;
+                        break;
                     case PUERTO_3:
-                        p3++;break;
+                        p3++;
+                        break;
                     default:
                         break;
                 }
             }
-            bufferedReader.close();
-            fileReader.close();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                bufferedReader.close();
+                fileReader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        System.out.println("ID OF THE HEAVIEST CONTAINER "+idPesoMax);
-        System.out.println("TOTAL WEIGHT " + df.format(pesoTotal)+" T ");
+        System.out.println("ID OF THE HEAVIEST CONTAINER " + idPesoMax);
+        System.out.println("TOTAL WEIGHT " + df.format(pesoTotal) + " T ");
         System.out.println("PORT 1 : " + p1);
         System.out.println("PORT 2 : " + p2);
         System.out.println("PORT 3 : " + p3);
