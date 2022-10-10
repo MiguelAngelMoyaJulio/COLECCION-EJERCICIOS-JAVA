e8e92a0426e239bdc55cf6a8070274aa5b371c45
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-10 13:47:43 -0300

    E7 AVERAGE AND SUMMATION REVIEW

--- EJERCICIOS/src/main/java/org/ar/miguel/STRUCTURES/E7.java
+++ EJERCICIOS/src/main/java/org/ar/miguel/STRUCTURES/E7.java
@@ -12,9 +12,13 @@ public class E7 {
         int sumPos = 0, sumNeg = 0;
         int cantPos = 0;
         int n = 0;
+        System.out.println("enter a number");
+        n = Integer.parseInt(s.nextLine());
+        result(n, sumPos, sumNeg, cantPos);
+    }
+
+    private static void result(int n, int sumPos, int sumNeg, int cantPos) {
         for (int i = 1; i <= 50; i++) {
-            System.out.println("enter a number");
-            n = Integer.parseInt(s.nextLine());
             if (n > 100) {
                 sumPos += n;
                 cantPos++;
@@ -23,6 +27,6 @@ public class E7 {
             }
         }
         System.out.println("summation of negative numbers " + sumNeg);
-        System.out.println("average of positive numbers " + sumPos/cantPos);
+        System.out.println("average of positive numbers " + sumPos / cantPos);
     }
 }
