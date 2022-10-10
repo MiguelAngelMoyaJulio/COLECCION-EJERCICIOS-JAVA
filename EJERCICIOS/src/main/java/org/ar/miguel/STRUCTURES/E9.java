c612ab1e3ea202262936f6c5ab744c01d6ffd794
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-10 16:46:58 -0300

    E9 MAX AND MIN REVIEW

--- EJERCICIOS/src/main/java/org/ar/miguel/STRUCTURES/E9.java
+++ EJERCICIOS/src/main/java/org/ar/miguel/STRUCTURES/E9.java
@@ -8,17 +8,12 @@ import java.util.Scanner;
  * ingresados cada uno de ellos.
  */
 public class E9 {
-    private static Scanner s = new Scanner(System.in);
-    private static int maxPos = 0, negPos = 0;
-    private static int n, num, max = 0, min = 999999999;
-
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int maxPos = 0, negPos = 0;
+        int n, num, max = 0, min = 999999999;
         System.out.println("enter the amount of numbers you want to enter");
         n = Integer.parseInt(s.nextLine());
-        process(n,num,max,min,maxPos,negPos,s);
-    }
-
-    private static void process(int n, int num, int max, int min,int maxPos, int negPos, Scanner s) {
         for (int i = 0; i < n; i++) {
             num = Integer.parseInt(s.nextLine());
             if (num > max) {
@@ -32,5 +27,6 @@ public class E9 {
         }
         System.out.println("Max " + max + " " + " position : " + (maxPos + 1));
         System.out.println("Min " + min + " " + " position : " + (negPos + 1));
+
     }
 }
