34c7e3dad2938deaf45cd9cd8a4c3550edb14da7
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-10 16:57:32 -0300

    E14 SHOW N ELEMENTS - FIBONACCI REVIEW

--- EJERCICIOS/src/main/java/org/ar/miguel/STRUCTURES/E14.java
+++ EJERCICIOS/src/main/java/org/ar/miguel/STRUCTURES/E14.java
@@ -11,8 +11,10 @@ public class E14 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n, currentNumber = 0, nextNumber = 1, aux = 0;
+
         System.out.println("enter a number ");
         n = Integer.parseInt(s.nextLine());
+
         for (int i = 0; i < n; i++) {
             System.out.println(currentNumber);
             aux = currentNumber + nextNumber;
