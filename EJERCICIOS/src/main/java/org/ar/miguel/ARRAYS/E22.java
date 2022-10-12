245b634a30e9805c89fe1d80170baaede9dc5362
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-12 09:20:27 -0300

    E22 ARRAY - EVEN AND ODD NUMBERS REVIEW

--- EJERCICIOS/src/main/java/org/ar/miguel/ARRAYS/E22.java
+++ EJERCICIOS/src/main/java/org/ar/miguel/ARRAYS/E22.java
@@ -17,34 +17,48 @@ public class E22 {
         n = Integer.parseInt(s.nextLine());
         if (n < 25) {
             arra = new int[n];
-            for (int i = 0; i < arra.length; i++) {
-                System.out.println("enter a number");
-                num = Integer.parseInt(s.nextLine());
-                arra[i] = num;
-            }
 
-            for (int i = 0; i < arra.length; i++) {
-                suma = suma + arra[i];
-            }
-
-            if (suma > 10) {
-                System.out.println("even index");
-                for (int i = 0; i < arra.length; i++) {
-                    if (i % 2 == 0) {
-                        System.out.println(arra[i]);
-                    }
-                }
-            } else {
-                System.out.println("odd index");
-                for (int i = 0; i < arra.length; i++) {
-                    if (i % 2 != 0) {
-                        System.out.println(arra[i]);
-                    }
-                }
-            }
+            loadVector(arra, s, num);
+            suma = sumVector(arra);
+            showOddEven(suma,arra);
         } else {
             System.out.println("Try again");
         }
 
     }
+
+    private static void showOddEven(int suma, int[] arra) {
+        if (suma > 10) {
+            System.out.println("even index");
+            for (int i = 0; i < arra.length; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(arra[i]);
+                }
+            }
+        } else {
+            System.out.println("odd index");
+            for (int i = 0; i < arra.length; i++) {
+                if (i % 2 != 0) {
+                    System.out.println(arra[i]);
+                }
+            }
+        }
+    }
+
+    private static int sumVector(int[] arra) {
+        int suma = 0;
+        for (int i = 0; i < arra.length; i++) {
+            suma = suma + arra[i];
+        }
+        return suma;
+    }
+
+    private static void loadVector(int[] arra, Scanner s, int num) {
+        for (int i = 0; i < arra.length; i++) {
+            System.out.println("enter a number");
+            num = Integer.parseInt(s.nextLine());
+            arra[i] = num;
+        }
+    }
+
 }
