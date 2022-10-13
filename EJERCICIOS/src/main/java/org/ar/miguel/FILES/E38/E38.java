063365c6c08ca9cf59be5e5155566885ff2bdd08
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-13 15:52:45 -0300

    E38 FILE - APPENDING NEW RECORDS REVIEW

--- EJERCICIOS/src/main/java/org/ar/miguel/FILES/E38/E38.java
+++ EJERCICIOS/src/main/java/org/ar/miguel/FILES/E38/E38.java
@@ -15,16 +15,22 @@ public class E38 {
     private static String FILE_OUTPUT = "D:\\Miguel\\DEVELOPMENT\\JAVA-EJERCICIOS-GIT\\EJERCICIOS\\src\\main\\java\\org\\ar\\miguel\\FILES\\E38\\ANTERIOR_DIA.txt";
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
+        //Put the exact location of the file Datos.csv
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+
         String readRecord = null;
         String[] record = new String[0];
         int i = 0;
+
         try {
-            FileWriter fileWriter = new FileWriter(FILE_OUTPUT, true);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            fileWriter = new FileWriter(FILE_OUTPUT, true);
+            bufferedWriter = new BufferedWriter(fileWriter);
             //Put the exact location of the file Datos.csv
-            FileReader fileReader = new FileReader(FILE_INPUT);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            fileReader = new FileReader(FILE_INPUT);
+            bufferedReader = new BufferedReader(fileReader);
 
             while ((readRecord = bufferedReader.readLine()) != null) {
                 if (i == 0) {
@@ -34,15 +40,18 @@ public class E38 {
                     bufferedWriter.write("\n" + readRecord);
                 }
             }
-
-            bufferedReader.close();
-            fileReader.close();
-
-            bufferedWriter.close();
-            fileWriter.close();
-
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                bufferedReader.close();
+                fileReader.close();
+
+                bufferedWriter.close();
+                fileWriter.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
