f1755e992881b60b9fb5c89f3d97f48e24c7bada
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-13 15:50:31 -0300

    E37 FILE - WRITE RECORD ACCORDING TO SUBJECT CODE REVIEW

--- EJERCICIOS/src/main/java/org/ar/miguel/FILES/E37/E37.java
+++ EJERCICIOS/src/main/java/org/ar/miguel/FILES/E37/E37.java
@@ -16,35 +16,43 @@ public class E37 {
     private static String FILE_OUTPUT = "D:\\Miguel\\DEVELOPMENT\\JAVA-EJERCICIOS-GIT\\EJERCICIOS\\src\\main\\java\\org\\ar\\miguel\\FILES\\E37\\SALIDA.txt";
 
     public static void main(String[] args) {
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
+        //Put the exact location of the file Datos.csv
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+
         Scanner s = new Scanner(System.in);
-        String readRecord = null;
+        String readRecord = null, codSubject = "";
         String[] record = new String[0];
-        String codSubject = "";
         System.out.println("enter subject's code ");
         codSubject = s.nextLine();
 
         try {
-            FileWriter fileWriter = new FileWriter(FILE_OUTPUT);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            fileWriter = new FileWriter(FILE_OUTPUT);
+            bufferedWriter = new BufferedWriter(fileWriter);
             //Put the exact location of the file Datos.csv
-            FileReader fileReader = new FileReader(FILE_INPUT);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            fileReader = new FileReader(FILE_INPUT);
+            bufferedReader = new BufferedReader(fileReader);
 
             while ((readRecord = bufferedReader.readLine()) != null) {
                 record = readRecord.split(";");
-                if(record[1].equalsIgnoreCase(codSubject)){
-                   bufferedWriter.write(readRecord+"\n");
+                if (record[1].equalsIgnoreCase(codSubject)) {
+                    bufferedWriter.write(readRecord + "\n");
                 }
             }
-
-            bufferedReader.close();
-            fileReader.close();
-
-            bufferedWriter.close();
-            fileWriter.close();
-
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                bufferedReader.close();
+                fileReader.close();
+
+                bufferedWriter.close();
+                fileWriter.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
