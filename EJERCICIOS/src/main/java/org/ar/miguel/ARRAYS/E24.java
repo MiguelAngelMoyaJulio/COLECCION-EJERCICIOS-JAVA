bc8913d8e151e3df4a2505098a22e91b6565afa6
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-13 13:32:06 -0300

    E24 ARRAY - MAXIMUM REVIEW

--- EJERCICIOS/src/main/java/org/ar/miguel/ARRAYS/E24.java
+++ EJERCICIOS/src/main/java/org/ar/miguel/ARRAYS/E24.java
@@ -14,32 +14,38 @@ public class E24 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int[] arra;
-        int n = 0, num = 0;
-        int max = 0;
+        int n = 0, num = 0, max = 0;
         System.out.println("enter the size of the array");
         n = Integer.parseInt(s.nextLine());
         if (n < 40) {
             arra = new int[n];
-            for (int i = 0; i < arra.length; i++) {
-                System.out.println("enter a number");
-                num = Integer.parseInt(s.nextLine());
-                arra[i] = num;
-            }
-            max = arra[0];
-            for (int i = 0; i < arra.length; i++) {
-                if (arra[i] > max) {
-                    max = arra[i];
-                }
-            }
-            for (int i = 0; i < arra.length; i++) {
-                if (arra[i] == max) {
-                    System.out.println("MAXIMUM " + max);
-                    System.out.println("POSITION MAXIMUM " + (i + 1));
-                }
-            }
+            loadArray(arra, s, num);
+            findMax(arra, max);
         } else {
             System.out.println("Try again");
         }
+    }
 
+    private static void loadArray(int[] arra, Scanner s, int num) {
+        for (int i = 0; i < arra.length; i++) {
+            System.out.println("enter a number");
+            num = Integer.parseInt(s.nextLine());
+            arra[i] = num;
+        }
+    }
+
+    private static void findMax(int[] arra, int max) {
+        max = arra[0];
+        for (int i = 0; i < arra.length; i++) {
+            if (arra[i] > max) {
+                max = arra[i];
+            }
+        }
+        for (int i = 0; i < arra.length; i++) {
+            if (arra[i] == max) {
+                System.out.println("MAXIMUM " + max);
+                System.out.println("POSITION MAXIMUM " + (i + 1));
+            }
+        }
     }
 }
